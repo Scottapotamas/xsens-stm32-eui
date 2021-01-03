e56c57d77f863763ed165eaf9e3ba930a46c56dd
Scott Rapson <scottapotamas@gmail.com>
2021-01-03 23:43:36 +1030

    Significant acceleration chart works properly, debounced

--- interface/src/application/pages/DevicePages/AccelerationHighlightChart.tsx
+++ interface/src/application/pages/DevicePages/AccelerationHighlightChart.tsx
@@ -8,6 +8,7 @@ import {
   TimeSlicedLineChart,
   RealTimeSlicingDomain,
   BlankingLineChart,
+  TriggerDomain,
 } from '@electricui/components-desktop-charts'
 import { Event } from '@electricui/timeseries'
 
@@ -28,13 +29,24 @@ const layoutDescription = `
 
 const accDS = new MessageDataSource('acc')
 
+let triggerMax: number = 0
+let lastTrigger: number = 0
+
 // Datasource which emits true when a strong acceleration occured
 // TODO: debounce timing
 // TODO: peak detection to decide if event is interesting, instead of threshold
 const triggerDS = new MessageDataSource('acc', (message, emit) => {
-  if (message.payload[0] > 10) {
-    const event = new Event(message.metadata.timestamp, 1)
-    emit(event)
+  // Debounce - only consider an event if we haven't recently
+  if (message.metadata.timestamp - lastTrigger > 5000) {
+    // Adaptive peak detection - only consider peaks larger
+    if (message.payload[0] > 6 || message.payload[0] > triggerMax) {
+      triggerMax = message.payload[0]
+      lastTrigger = message.metadata.timestamp
+
+      // Generate the trigger event
+      const event = new Event(message.metadata.timestamp + 5000, 1)
+      emit(event)
+    }
   }
 })
 
@@ -46,28 +58,33 @@ export const AccelerationHighlightChart = () => {
           <React.Fragment>
             <Areas.Title>
               <div style={{ textAlign: 'left', marginLeft: '4em' }}>
-                <b>SIGNIFICANT ACCELERATION</b>
+                <b>SIGNIFICANT ACCELERATION TRACE</b>
               </div>
             </Areas.Title>
 
             <Areas.Chart>
               <ChartContainer height="30vh">
-                <BlankingLineChart
+                <LineChart
                   dataSource={accDS}
-                  yAccessor={event => event[0]}
-                  triggerDataSource={triggerDS}
-                  width={1000}
+                  accessor={event => event[0]}
+                  color={Colors.GREEN4}
+                />
+                <LineChart
+                  dataSource={accDS}
+                  accessor={event => event[1]}
+                  color={Colors.BLUE4}
+                />
+                <LineChart
+                  dataSource={accDS}
+                  accessor={event => event[2]}
                   color={Colors.RED4}
                 />
                 <TimeAxis />
-                <VerticalAxis label="(units)" />
-                {/* <RealTimeDomain window={10000} /> */}
-                <RealTimeSlicingDomain
-                  xMin={0}
-                  xMax={1000}
-                  yMin={-1}
-                  yMax={1}
+                <VerticalAxis label="Acceleration m/s²" />
+                <TriggerDomain
                   window={10000}
+                  dataSource={triggerDS}
+                  accessor={(event, time) => time}
                 />
               </ChartContainer>
             </Areas.Chart>
